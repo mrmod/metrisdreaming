2d31700b289ea5b15a4c9960716429cad2e3b0dc
mrmod <iambruce@me.com>
2020-09-03 08:55:10 -0700

    Utilize Algolia when possible
    Some of the dealerships use XHR for results and jQuery to put the results on the page. The dealerships/DealerInspire don't use security on their Algolia connections so they're free to re-use

---
+++ src/dealerships.js
@@ -0,0 +1,92 @@
+
+import bakersfieldConfig from './bakersfield'
+import santaRosaConfig from './santaRosa'
+
+export default [
+    {
+        name: 'Pleasonton',
+        url: 'https://www.mbofpleasanton.com/used-cars-pleasanton-ca.html?Make=Mercedes-Benz',
+    },
+    {
+        name: 'San Jose',
+        url: 'https://www.mercedesbenzofsanjose.net/used-cars-san-jose-ca?_gmod%5B0%5D=Dfe_Modules_VehiclePrice_Module&_gmod%5B1%5D=Dfe_Modules_CustomizePayment_Module&direction=asc&t=u&make[]=Mercedes-Benz',
+    },
+    {
+        name: 'Stevens Creek',
+        url: 'https://www.mbofstevenscreek.net/used-cars-san-jose-ca?_gmod%5B0%5D=Dfe_Modules_VehiclePrice_Module&_gmod%5B1%5D=Dfe_Modules_CustomizePayment_Module&direction=asc&t=u&make[]=Mercedes-Benz&sf=sf_make',
+    },
+    {
+        name: 'Monterey',
+        url: 'https://express.montereymercedes.com/inventory/used?deal_type=cash&f=make%3AMercedes-Benz&sort=lowest%20price',
+    },
+    {
+        name: 'Fremont (Fletcher Jones)',
+        url: 'https://www.mboffremont.com/used-vehicles/#action=im_ajax_call&perform=get_results&make%5B%5D=Mercedes-Benz&page=1',
+    },
+    {
+        name: 'Walnut Creek',
+        url: 'https://www.mbofwalnutcreek.com/used-cars/walnut-creek.htm?search=&saveFacetState=true&make=Mercedes-Benz&lastFacetInteracted=inventory-listing1-facet-anchor-make-20',
+    },
+    {
+        name: 'San Francisco',
+        url: 'https://express.sfbenz.com/inventory/used?deal_type=cash&f=make%3AMercedes-Benz',
+    },
+    {
+        name: 'Marin',
+        url: 'https://www.mbofmarin.com/searchused.aspx?Make=Mercedes-Benz',
+    },
+    {
+        name: 'Santa Rosa',
+        url: 'https://www.mbofsantarosa.com/used-vehicles/?_dFR%5Bmake%5D%5B0%5D=Mercedes-Benz&_dFR%5Btype%5D%5B0%5D=Pre-Owned&_dFR%5Btype%5D%5B1%5D=Certified%2520Pre-Owned&_paymentType=our_price',
+        config: santaRosaConfig,
+    },
+    {
+        name: 'Fairfield',
+        url: 'https://express.mercedesfairfield.com/inventory/used?deal_type=finance&f=make%3AMercedes-Benz',
+    },
+    {
+        name: 'Modesto',
+        url: 'https://express.mbofmodesto.com/inventory/used?deal_type=cash&f=make%3AMercedes-Benz',
+    },
+    {
+        name: 'Portland, OR',
+        url: 'https://www.mercedesbenzportland.com/used-vehicles/#action=im_ajax_call&perform=get_results&make%5B%5D=Mercedes-Benz&page=1',
+    },
+    {
+        name: 'Eugene, OR',
+        url: 'https://www.mbeugene.com/used-vehicles/#action=im_ajax_call&perform=get_results&make%5B%5D=Mercedes-Benz&page=1',
+    },
+    {
+        name: 'Medford, OR',
+        url: 'https://www.mbmedford.com/pre-owned-cars-medford-or?_gmod%5B0%5D=Dfe_Modules_VehiclePrice_Module&_gmod%5B1%5D=Dfe_Modules_CustomizePayment_Module&direction=asc&t=u&make[]=Mercedes-Benz&sf=sf_make',
+    },
+    {
+        name: 'Bend, OR',
+        url: 'https://www.mbofbend.com/used-vehicles/#action=im_ajax_call&perform=get_results&make%5B%5D=Mercedes-Benz&page=1',
+    },
+    {
+        name: 'Sacremento',
+        url: 'https://www.mbsacramento.com/pre-owned-vehicles/#action=im_ajax_call&perform=get_results&make%5B%5D=Mercedes-Benz&page=1',
+    },
+    {
+        name: 'Reno, NV',
+        url: 'https://www.mercedesbenzofreno.net/used-cars-reno-nv?_gmod%5B0%5D=Dfe_Modules_VehiclePrice_Module&_gmod%5B1%5D=Dfe_Modules_CustomizePayment_Module&direction=asc&t=u&make[]=Mercedes-Benz&sf=sf_make',
+    },
+    {
+        name: 'Fresno',
+        url: 'https://www.mboffresno.com/used-inventory/index.htm?listingConfigId=auto-used&year=&make=Mercedes-Benz&bodyStyle=&odometer=&internetPrice=&start=0&sort=&facetbrowse=true&searchLinkText=SEARCH&showInvTotals=false&showRadius=false&showReset=false&showSubmit=true&facetbrowseGridUnit=BLANK&showSelections=true&dependencies=model%3Amake%2Ccity%3Aprovince%2Ccity%3Astate&suppressAllConditions=true',
+    },
+    {
+        name: 'Bakersfield',
+        url: 'https://www.mbofbakersfield.com/used-vehicles/?_dFR%5Bmake%5D%5B0%5D=Mercedes-Benz&_dFR%5Btype%5D%5B0%5D=Pre-Owned&_dFR%5Btype%5D%5B1%5D=Certified%2520Pre-Owned&_paymentType=our_price',
+        config: bakersfieldConfig,
+    },
+    {
+        name: 'Santa Barbara',
+        url: 'https://www.mercedessantabarbara.com/used-vehicles/#action=im_ajax_call&perform=get_results&make%5B%5D=Mercedes-Benz&page=1',
+    },
+    {
+        name: 'El Dorado Hills',
+        url: 'https://www.mbofedh.com/used-vehicles/#action=im_ajax_call&perform=get_results&make%5B%5D=Mercedes-Benz&page=1',
+    }
+]
